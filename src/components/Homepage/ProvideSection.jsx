b38ed77852a03211c496ee82202b55f693b751cd
Hasan Alam <hasanalam365@gmail.com>
2024-11-20 20:46:56 +0600

    provide page done

--- src/components/Homepage/ProvideSection.jsx
+++ src/components/Homepage/ProvideSection.jsx
@@ -1,45 +1,53 @@
+'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCircleCheck } from "react-icons/fa6";
 
 const ProvideSection = () => {
-    return (
-        <div className='flex  justify-between gap-8  mx-8 mt-8'>
-            <div className='flex-1'>
-                <h2 className='text-xl font-semibold mb-8'>We provide...</h2>
-               <div className="collapse bg-base-200 ">
-  <input type="radio" name="my-accordion-1" defaultChecked />
-  <div className="collapse-title text-xl font-medium">Personalized routine</div>
-  <div className="collapse-content">
-    <p>We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively.</p>
-  </div>
-</div>
-<div className="collapse bg-base-200 mt-3">
-  <input type="radio" name="my-accordion-1" />
-  <div className="collapse-title text-xl font-medium">Follow-up after completing courses</div>
-  <div className="collapse-content">
-    <p>We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively.</p>
-  </div>
-</div>
-<div className="collapse bg-base-200 mt-3">
-  <input type="radio" name="my-accordion-1" />
-  <div className="collapse-title text-xl font-medium">Access to additional resources</div>
-  <div className="collapse-content">
-    <p>We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively.</p>
-  </div>
-</div>
-<div className="collapse bg-base-200 mt-3">
-  <input type="radio" name="my-accordion-1" />
-  <div className="collapse-title text-xl font-medium">Free community support</div>
-  <div className="collapse-content">
-    <p>We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively.</p>
-                    </div>
-                  
-                </div>
-                  <button className="btn btn-outline text-primary mt-5 rounded-full">Learn more about our services</button>
+   
+    const [isOpenService, setIsOpenService] = useState(0);
+    
+    const allServices=[
+                    { id: 1, title: "Personalized routine", content: "We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively." },
+                    { id: 2, title: "Follow-up after completing courses", content: "We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively." },
+                    { id: 3, title: "Access to additional resources", content: "We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively." },
+                    { id: 4, title: "Free community support", content: "We offer access to a variety of additional resources to enhance your experience, including exclusive tools, guides, and support materials designed to help you achieve your goals more effectively." }
+                ]
 
+    return (
+        <div className="flex justify-between gap-8 mx-8 mt-16">
+            {/* Left Section */}
+            <div className="flex-1">
+                <h2 className="text-xl font-semibold mb-8">We provide...</h2>
+
+                {/* Accordion Items */}
+                {allServices.map((service, index) => (
+                    <div key={service.id} className={`collapse bg-base-200 ${index > 0 ? 'mt-3' : ''} `}
+                    onClick={()=>setIsOpenService(index)}>
+                        <input
+                            type="radio"
+                            name="my-accordion-1"
+                            defaultChecked={index === 0}
+                        />
+                        <div className="collapse-title text-xl font-medium flex items-center gap-4">
+                            <FaCircleCheck className={`${isOpenService=== index? 'text-[#9998E1]' : ''}`} />
+                            <h4>{service.title}</h4>
+                        </div>
+                        <div className="collapse-content">
+                            <p>{service.content}</p>
+                        </div>
+                    </div>
+                ))}
+
+                {/* Learn More Button */}
+                <button className="btn btn-outline text-primary mt-5 rounded-full">
+                    Learn more about our services
+                </button>
             </div>
-            <div className=''>
-                <Image src="/provide.png" alt='provide image' width={450} height={350}/>
+
+            {/* Right Section */}
+            <div>
+                <Image src="/provide.png" alt="Provide Image" width={450} height={350} />
             </div>
         </div>
     );
