55eda5d2da54f5f9ae2d9bf909b6d00c25d88484
Hasan Alam <hasanalam365@gmail.com>
2024-11-21 12:37:58 +0600

    hover course card then bg change

--- src/components/Homepage/PopularCourses.jsx
+++ src/components/Homepage/PopularCourses.jsx
@@ -42,7 +42,7 @@ const PopularCourses = async () => {
             {/* Courses Grid */}
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {allCourses.map((course, index) => (
-                    <div key={index} className="card card-compact bg-base-100 shadow-xl">
+                    <div key={index} className="card card-compact bg-base-100 shadow-xl hover:bg-[#dbdbf186]">
                         {/* Card Image */}
                         <figure>
                             <Image
